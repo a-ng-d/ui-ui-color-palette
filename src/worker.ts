f72f9e8eb313b0b05dabb8595fcc80e4d5af2921
Aurélien Grimaud <a.grimaud@involt.io>
2024-09-01 14:35:46 +0200

    feat: Add support for retrieving announcements and version from Notion database
    The code changes in this commit add support for retrieving announcements and version information from a Notion database. The `fetch` function now checks the `action` parameter in the URL to determine whether to retrieve the version or announcements. If the action is "get_version", it retrieves the version information from the database. If the action is "get_announcements", it retrieves the announcements that are marked as published.
    
    This commit also updates the project name and dependencies, as indicated in the recent user commits.

--- src/worker.ts
+++ src/worker.ts
@@ -2,53 +2,113 @@ import { Client, APIErrorCode } from '@notionhq/client'
 
 export default {
   async fetch(request, env, ctx) {
-    try {
-      const notion = new Client({
-        auth: env.NOTION_API_KEY,
-      })
+    const url = new URL(request.url)
 
-      const announcementsList = await notion.databases.query({
-        database_id: 'e078993c1ac74bdb8d2806174927ddcb',
-        filter: {
-          property: 'Publié',
-          checkbox: {
-            equals: true,
+    if (url.searchParams.get('action') === 'get_version') {
+      try {
+        const notion = new Client({
+          auth: env.NOTION_API_KEY,
+        })
+
+        const announcementsInfo = await notion.databases.retrieve({
+          database_id: url.searchParams.get('database_id') ?? 'e078993c1ac74bdb8d2806174927ddcb',
+        })
+
+        return new Response(
+          JSON.stringify({
+            version: announcementsInfo.description[0].plain_text as string,
+          }) as BodyInit,
+          {
+            status: 200,
+            headers: {
+              'Access-Control-Allow-Origin': '*',
+            },
+          }
+        )
+      } catch (error) {
+        if (error.code === APIErrorCode.ObjectNotFound) {
+          return new Response(
+            JSON.stringify({
+              message: 'The database is not found',
+            }) as BodyInit,
+            {
+              status: 404,
+              headers: {
+                'Access-Control-Allow-Origin': '*',
+              },
+            }
+          )
+        } else {
+          console.error(error)
+          return new Response(
+            JSON.stringify({
+              message: 'The database could not be queried',
+            }) as BodyInit,
+            {
+              status: 400,
+              headers: {
+                'Access-Control-Allow-Origin': '*',
+              },
+            }
+          )
+        }
+      }
+    }
+
+    if (url.searchParams.get('action') === 'get_announcements') {
+      try {
+        const notion = new Client({
+          auth: env.NOTION_API_KEY,
+        })
+
+        const announcementsList = await notion.databases.query({
+          database_id: url.searchParams.get('database_id') ?? 'e078993c1ac74bdb8d2806174927ddcb',
+          filter: {
+            property: 'Publié',
+            checkbox: {
+              equals: true,
+            },
           },
-        },
-      })
+        })
 
-      return new Response(JSON.stringify(announcementsList.results) as BodyInit, {
-        status: 200,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
-      })
-    } catch (error) {
-      if (error.code === APIErrorCode.ObjectNotFound) {
         return new Response(
           JSON.stringify({
-            message: 'The database is not found',
+            anouncements: announcementsList,
           }) as BodyInit,
           {
-            status: 404,
+            status: 200,
             headers: {
               'Access-Control-Allow-Origin': '*',
             },
           }
         )
-      } else {
-        console.error(error)
-        return new Response(
-          JSON.stringify({
-            message: 'The database could not be queried',
-          }) as BodyInit,
-          {
-            status: 400,
-            headers: {
-              'Access-Control-Allow-Origin': '*',
-            },
-          }
-        )
+      } catch (error) {
+        if (error.code === APIErrorCode.ObjectNotFound) {
+          return new Response(
+            JSON.stringify({
+              message: 'The database is not found',
+            }) as BodyInit,
+            {
+              status: 404,
+              headers: {
+                'Access-Control-Allow-Origin': '*',
+              },
+            }
+          )
+        } else {
+          console.error(error)
+          return new Response(
+            JSON.stringify({
+              message: 'The database could not be queried',
+            }) as BodyInit,
+            {
+              status: 400,
+              headers: {
+                'Access-Control-Allow-Origin': '*',
+              },
+            }
+          )
+        }
       }
     }
   },
