a577809a960626924c81144bc8ad5964ca7c4420
Aurélien Grimaud <a.grimaud@involt.io>
2025-06-08 02:03:15 +0200

    fix: restore checkEditorType function structure and comment out postMessage logic

--- src/bridges/checks/checkEditorType.ts
+++ src/bridges/checks/checkEditorType.ts
@@ -1,3 +1,14 @@
-const checkEditorType = async () => {}
+const checkEditorType = async () => {
+  const iframe = document.querySelector(
+    '#ui-container'
+  ) as HTMLIFrameElement | null
+
+  /*iframe?.contentWindow?.postMessage({
+    type: 'CHECK_EDITOR_TYPE',
+    data: {
+      editor: 'dev_vscode'
+    },
+  })*/
+}
 
 export default checkEditorType
