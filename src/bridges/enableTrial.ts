7fa3e9670fbab3e86f55e4af16170085d7eb02ec
Aurélien Grimaud <a.grimaud@involt.io>
2025-06-17 16:14:01 +0200

    refactor: rename variable 'date' to 'now' for clarity in enableTrial function

--- src/bridges/enableTrial.ts
+++ src/bridges/enableTrial.ts
@@ -1,17 +1,17 @@
 const enableTrial = async (trialTime: number, trialVersion: string) => {
-  const date = new Date().getTime()
+  const now = new Date().getTime()
   const iframe = document.querySelector(
     '#ui-container'
   ) as HTMLIFrameElement | null
 
-  window.localStorage.setItem('trial_start_date', date.toString())
+  window.localStorage.setItem('trial_start_date', now.toString())
   window.localStorage.setItem('trial_version', trialVersion)
   window.localStorage.setItem('trial_time', trialTime.toString())
 
   return iframe?.contentWindow?.postMessage({
     type: 'ENABLE_TRIAL',
     data: {
-      date: date,
+      date: now,
       trialTime: trialTime,
     },
   })
