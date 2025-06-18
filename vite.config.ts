f4f28b5c6845c9a7e78024b453d39f6c011e2752
Aurélien Grimaud <a.grimaud@involt.io>
2025-06-18 20:45:14 +0200

    refactor: remove unused alias for react-dom/test-utils in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -30,7 +30,6 @@ export default defineConfig(({ mode }) => {
     resolve: {
       alias: {
         react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
         'react-dom': 'preact/compat',
         'react/jsx-runtime': 'preact/jsx-runtime',
       },
