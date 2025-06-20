5147b8dbe17d728135e10a1ca78c4638fa846acc
Aurélien Grimaud <a.grimaud@involt.io>
2025-06-20 23:52:13 +0200

    feat: refactor pullPalette to update palette data structure and improve message handling

--- src/external/publication/pullPalette.ts
+++ src/external/publication/pullPalette.ts
@@ -19,48 +19,86 @@ const pullPalette = async ({
     .eq('palette_id', rawData.id)
 
   if (!error && data.length === 1) {
-    const palette: FullConfiguration = new Data({
-      base: {
-        name: data[0].name,
-        description: data[0].description,
-        preset: data[0].preset,
-        shift: data[0].shift,
-        areSourceColorsLocked: data[0].are_source_colors_locked,
-        colors: data[0].colors,
-        colorSpace: data[0].color_space,
-        algorithmVersion: data[0].algorithm_version,
-      },
-      themes: data[0].themes,
-      meta: {
-        id: data[0].palette_id,
-        dates: {
-          createdAt: data[0].created_at,
-          updatedAt: data[0].updated_at,
-          publishedAt: data[0].published_at,
-          openedAt: rawData.dates.openedAt,
-        },
-        publicationStatus: {
-          isPublished: true,
-          isShared: data[0].is_shared,
-        },
-        creatorIdentity: {
-          creatorId: data[0].creator_id,
-          creatorFullName: data[0].creator_full_name,
-          creatorAvatar: data[0].creator_avatar,
-        },
-      },
-    }).makePaletteFullData()
-
     parent.postMessage(
       {
         pluginMessage: {
-          type: 'SET_DATA',
+          type: 'UPDATE_PALETTE',
+          id: data[0].palette_id,
           items: [
             {
-              key: `palette_${data[0].palette_id}`,
-              value: palette,
+              key: 'base.name',
+              value: data[0].name,
+            },
+            {
+              key: 'base.description',
+              value: data[0].description,
+            },
+            {
+              key: 'base.preset',
+              value: data[0].preset,
+            },
+            {
+              key: 'base.shift',
+              value: data[0].shift,
+            },
+            {
+              key: 'base.areSourceColorsLocked',
+              value: data[0].are_source_colors_locked,
+            },
+            {
+              key: 'base.colors',
+              value: data[0].colors,
+            },
+            {
+              key: 'base.colorSpace',
+              value: data[0].color_space,
+            },
+            {
+              key: 'base.algorithmVersion',
+              value: data[0].algorithm_version,
+            },
+            {
+              key: 'themes',
+              value: data[0].themes,
+            },
+            {
+              key: 'meta.dates.publishedAt',
+              value: data[0].published_at,
+            },
+            {
+              key: 'meta.dates.createdAt',
+              value: data[0].created_at,
+            },
+            {
+              key: 'meta.dates.updatedAt',
+              value: data[0].updated_at,
+            },
+            {
+              key: 'meta.dates.openedAt',
+              value: rawData.dates.openedAt,
+            },
+            {
+              key: 'meta.publicationStatus.isPublished',
+              value: true,
+            },
+            {
+              key: 'meta.publicationStatus.isShared',
+              value: data[0].is_shared,
+            },
+            {
+              key: 'meta.creatorIdentity.creatorId',
+              value: data[0].creator_id,
+            },
+            {
+              key: 'meta.creatorIdentity.creatorFullName',
+              value: data[0].creator_full_name,
+            },
+            {
+              key: 'meta.creatorIdentity.creatorAvatar',
+              value: data[0].creator_avatar,
             },
           ],
+          isAlreadyUpdated: true,
         },
       },
       '*'
