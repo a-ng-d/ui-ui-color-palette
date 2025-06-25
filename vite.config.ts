547035b873d4ece675e9d9a931019ccf430f4d2f
Aurélien Grimaud <a.grimaud@involt.io>
2025-06-25 14:47:09 +0200

    refactor: remove unused keywords and dependencies from package.json; update vite.config.ts to disable basic SSL plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig, loadEnv } from 'vite'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 import { sentryVitePlugin } from '@sentry/vite-plugin'
 import preact from '@preact/preset-vite'
 
@@ -18,7 +17,6 @@ export default defineConfig(({ mode }) => {
         project: 'ui-color-palette',
         authToken: env.SENTRY_AUTH_TOKEN,
       }),
-      // basicSsl(),
     ],
 
     define: {
