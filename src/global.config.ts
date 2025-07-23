06d25d8242be2a5c96ff1ff18d8df70d2f373f1e
Aurélien Grimaud <a.grimaud@involt.io>
2025-07-23 17:22:28 +0200

    fix: update URLs for ISB and UICP to point to the website

--- src/global.config.ts
+++ src/global.config.ts
@@ -547,8 +547,8 @@ const globalConfig: Config = {
     privacyUrl: 'https://uicp.ylb.lt/privacy',
     vsCodeFigmaPluginUrl:
       'https://marketplace.visualstudio.com/items?itemName=figma.figma-vscode-extension',
-    isbUrl: 'https://isb.ylb.lt/run-figma',
-    uicpUrl: 'https://uicp.ylb.lt/run-figma-one',
+    isbUrl: 'https://isb.ylb.lt/website',
+    uicpUrl: 'https://uicp.ylb.lt/website',
     storeUrl: isDev
       ? 'https://uicp.ylb.lt/store-dev'
       : 'https://uicp.ylb.lt/store',
