50390e9a4ee4ea572e76dd413925da06af5607c6
Aurélien Grimaud <a.grimaud@involt.io>
2025-11-15 23:22:03 +0100

    feat: Integrate feature flag for user language selection in LangPreferences component

--- src/ui/modules/LangPreferences.tsx
+++ src/ui/modules/LangPreferences.tsx
@@ -9,6 +9,7 @@ import {
   SimpleItem,
 } from '@a_ng_d/figmug-ui'
 import { WithConfigProps } from '../components/WithConfig'
+import Feature from '../components/Feature'
 import { Language } from '../../types/translations'
 import { BaseProps, Editor, PlanStatus, Service } from '../../types/app'
 import { $userLanguage } from '../../stores/preferences'
@@ -96,144 +97,153 @@ export default class LangPreferences extends PureComponent<
   // Render
   render() {
     return (
-      <Section
-        title={
-          <SimpleItem
-            leftPartSlot={
-              <SectionTitle label={this.props.locales.user.language.title} />
-            }
-            isListItem={false}
-            alignment="CENTER"
-          />
-        }
-        body={[
-          {
-            node: (
-              <FormItem
-                id="user-language"
-                label={this.props.locales.user.language.label}
-                shouldFill
-              >
-                <Dropdown
+      <Feature
+        isActive={LangPreferences.features(
+          this.props.planStatus,
+          this.props.config,
+          this.props.service,
+          this.props.editor
+        ).USER_LANGUAGE.isActive()}
+      >
+        <Section
+          title={
+            <SimpleItem
+              leftPartSlot={
+                <SectionTitle label={this.props.locales.user.language.title} />
+              }
+              isListItem={false}
+              alignment="CENTER"
+            />
+          }
+          body={[
+            {
+              node: (
+                <FormItem
                   id="user-language"
-                  options={[
-                    {
-                      label: this.props.locales.user.language.englishUS,
-                      value: 'en-US',
-                      type: 'OPTION' as const,
-                      isActive: LangPreferences.features(
-                        this.props.planStatus,
-                        this.props.config,
-                        this.props.service,
-                        this.props.editor
-                      ).USER_LANGUAGE_EN_US.isActive(),
-                      isBlocked: LangPreferences.features(
-                        this.props.planStatus,
-                        this.props.config,
-                        this.props.service,
-                        this.props.editor
-                      ).USER_LANGUAGE_EN_US.isBlocked(),
-                      isNew: LangPreferences.features(
-                        this.props.planStatus,
-                        this.props.config,
-                        this.props.service,
-                        this.props.editor
-                      ).USER_LANGUAGE_EN_US.isNew(),
-                      action: () => $userLanguage.set('en-US'),
-                    },
-                    {
-                      label: this.props.locales.user.language.chineseCN,
-                      value: 'zh-CN',
-                      type: 'OPTION' as const,
-                      isActive: LangPreferences.features(
-                        this.props.planStatus,
-                        this.props.config,
-                        this.props.service,
-                        this.props.editor
-                      ).USER_LANGUAGE_ZH_CN.isActive(),
-                      isBlocked: LangPreferences.features(
-                        this.props.planStatus,
-                        this.props.config,
-                        this.props.service,
-                        this.props.editor
-                      ).USER_LANGUAGE_ZH_CN.isBlocked(),
-                      isNew: LangPreferences.features(
-                        this.props.planStatus,
-                        this.props.config,
-                        this.props.service,
-                        this.props.editor
-                      ).USER_LANGUAGE_ZH_CN.isNew(),
-                      action: () => $userLanguage.set('zh-CN'),
-                    },
-                    {
-                      label: this.props.locales.user.language.frenchFR,
-                      value: 'fr-FR',
-                      type: 'OPTION' as const,
-                      isActive: LangPreferences.features(
-                        this.props.planStatus,
-                        this.props.config,
-                        this.props.service,
-                        this.props.editor
-                      ).USER_LANGUAGE_FR_FR.isActive(),
-                      isBlocked: LangPreferences.features(
-                        this.props.planStatus,
-                        this.props.config,
-                        this.props.service,
-                        this.props.editor
-                      ).USER_LANGUAGE_FR_FR.isBlocked(),
-                      isNew: LangPreferences.features(
-                        this.props.planStatus,
-                        this.props.config,
-                        this.props.service,
-                        this.props.editor
-                      ).USER_LANGUAGE_FR_FR.isNew(),
-                      action: () => $userLanguage.set('fr-FR'),
-                    },
-                    {
-                      label: this.props.locales.user.language.portugueseBR,
-                      value: 'pt-BR',
-                      type: 'OPTION' as const,
-                      isActive: LangPreferences.features(
-                        this.props.planStatus,
-                        this.props.config,
-                        this.props.service,
-                        this.props.editor
-                      ).USER_LANGUAGE_PT_BR.isActive(),
-                      isBlocked: LangPreferences.features(
-                        this.props.planStatus,
-                        this.props.config,
-                        this.props.service,
-                        this.props.editor
-                      ).USER_LANGUAGE_PT_BR.isBlocked(),
-                      isNew: LangPreferences.features(
-                        this.props.planStatus,
-                        this.props.config,
-                        this.props.service,
-                        this.props.editor
-                      ).USER_LANGUAGE_PT_BR.isNew(),
-                      action: () => $userLanguage.set('pt-BR'),
-                    },
-                  ]}
-                  selected={this.state.userLanguage}
-                  isBlocked={LangPreferences.features(
-                    this.props.planStatus,
-                    this.props.config,
-                    this.props.service,
-                    this.props.editor
-                  ).USER_LANGUAGE.isBlocked()}
-                  isNew={LangPreferences.features(
-                    this.props.planStatus,
-                    this.props.config,
-                    this.props.service,
-                    this.props.editor
-                  ).USER_LANGUAGE.isNew()}
-                />
-              </FormItem>
-            ),
-          },
-        ]}
-        border={!this.props.isLast ? ['BOTTOM'] : undefined}
-      />
+                  label={this.props.locales.user.language.label}
+                  shouldFill
+                >
+                  <Dropdown
+                    id="user-language"
+                    options={[
+                      {
+                        label: this.props.locales.user.language.englishUS,
+                        value: 'en-US',
+                        type: 'OPTION' as const,
+                        isActive: LangPreferences.features(
+                          this.props.planStatus,
+                          this.props.config,
+                          this.props.service,
+                          this.props.editor
+                        ).USER_LANGUAGE_EN_US.isActive(),
+                        isBlocked: LangPreferences.features(
+                          this.props.planStatus,
+                          this.props.config,
+                          this.props.service,
+                          this.props.editor
+                        ).USER_LANGUAGE_EN_US.isBlocked(),
+                        isNew: LangPreferences.features(
+                          this.props.planStatus,
+                          this.props.config,
+                          this.props.service,
+                          this.props.editor
+                        ).USER_LANGUAGE_EN_US.isNew(),
+                        action: () => $userLanguage.set('en-US'),
+                      },
+                      {
+                        label: this.props.locales.user.language.chineseCN,
+                        value: 'zh-CN',
+                        type: 'OPTION' as const,
+                        isActive: LangPreferences.features(
+                          this.props.planStatus,
+                          this.props.config,
+                          this.props.service,
+                          this.props.editor
+                        ).USER_LANGUAGE_ZH_CN.isActive(),
+                        isBlocked: LangPreferences.features(
+                          this.props.planStatus,
+                          this.props.config,
+                          this.props.service,
+                          this.props.editor
+                        ).USER_LANGUAGE_ZH_CN.isBlocked(),
+                        isNew: LangPreferences.features(
+                          this.props.planStatus,
+                          this.props.config,
+                          this.props.service,
+                          this.props.editor
+                        ).USER_LANGUAGE_ZH_CN.isNew(),
+                        action: () => $userLanguage.set('zh-CN'),
+                      },
+                      {
+                        label: this.props.locales.user.language.frenchFR,
+                        value: 'fr-FR',
+                        type: 'OPTION' as const,
+                        isActive: LangPreferences.features(
+                          this.props.planStatus,
+                          this.props.config,
+                          this.props.service,
+                          this.props.editor
+                        ).USER_LANGUAGE_FR_FR.isActive(),
+                        isBlocked: LangPreferences.features(
+                          this.props.planStatus,
+                          this.props.config,
+                          this.props.service,
+                          this.props.editor
+                        ).USER_LANGUAGE_FR_FR.isBlocked(),
+                        isNew: LangPreferences.features(
+                          this.props.planStatus,
+                          this.props.config,
+                          this.props.service,
+                          this.props.editor
+                        ).USER_LANGUAGE_FR_FR.isNew(),
+                        action: () => $userLanguage.set('fr-FR'),
+                      },
+                      {
+                        label: this.props.locales.user.language.portugueseBR,
+                        value: 'pt-BR',
+                        type: 'OPTION' as const,
+                        isActive: LangPreferences.features(
+                          this.props.planStatus,
+                          this.props.config,
+                          this.props.service,
+                          this.props.editor
+                        ).USER_LANGUAGE_PT_BR.isActive(),
+                        isBlocked: LangPreferences.features(
+                          this.props.planStatus,
+                          this.props.config,
+                          this.props.service,
+                          this.props.editor
+                        ).USER_LANGUAGE_PT_BR.isBlocked(),
+                        isNew: LangPreferences.features(
+                          this.props.planStatus,
+                          this.props.config,
+                          this.props.service,
+                          this.props.editor
+                        ).USER_LANGUAGE_PT_BR.isNew(),
+                        action: () => $userLanguage.set('pt-BR'),
+                      },
+                    ]}
+                    selected={this.state.userLanguage}
+                    isBlocked={LangPreferences.features(
+                      this.props.planStatus,
+                      this.props.config,
+                      this.props.service,
+                      this.props.editor
+                    ).USER_LANGUAGE.isBlocked()}
+                    isNew={LangPreferences.features(
+                      this.props.planStatus,
+                      this.props.config,
+                      this.props.service,
+                      this.props.editor
+                    ).USER_LANGUAGE.isNew()}
+                  />
+                </FormItem>
+              ),
+            },
+          ]}
+          border={!this.props.isLast ? ['BOTTOM'] : undefined}
+        />
+      </Feature>
     )
   }
 }
