5b70da7a9112d7d412da9ecf0258fd267aaf5b8e
Aurélien Grimaud <a.grimaud@involt.io>
2025-12-01 17:09:29 +0100

    fix: update creditsLimit and creditsVersion in global configuration refactor: remove unused userLanguage atom from preferences store

--- src/stores/preferences.ts
+++ src/stores/preferences.ts
@@ -1,5 +1,4 @@
 import { atom } from 'nanostores'
-import { Language } from '../types/translations'
 
 export const $isWCAGDisplayed = atom<boolean>(true)
 export const $isAPCADisplayed = atom<boolean>(true)
@@ -8,4 +7,3 @@ export const $canVariablesDeepSync = atom<boolean>(false)
 export const $isVsCodeMessageDisplayed = atom<boolean>(true)
 export const $pluginWindowWidth = atom<number>(640)
 export const $pluginWindowHeight = atom<number>(400)
-export const $userLanguage = atom<Language>('en-US')
