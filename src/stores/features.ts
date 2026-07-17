3481104f6613563d9fcf638ec60f29339426fd1f
Aurélien Grimaud <a.grimaud@involt.io>
2026-07-17 18:19:05 +0200

    feat: update availabilityForEditors in featuresScheme to remove framer

--- src/stores/features.ts
+++ src/stores/features.ts
@@ -3547,7 +3547,7 @@ export const featuresScheme: Array<Feature<Service>> = [
     type: 'ACTION',
     availabilityForServices: ['MANAGE', 'GEN', 'EXTRACT', 'WHEEL', 'EXPLORE'],
     proForServices: ['MANAGE', 'GEN', 'EXTRACT', 'WHEEL', 'EXPLORE'],
-    availabilityForEditors: ['figma', 'sketch', 'framer'],
+    availabilityForEditors: ['figma', 'sketch'],
     limit: 0,
   },
   {
