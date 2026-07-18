ef5c5fd1151dc814b079ac4d2901d1ae30b27649
Aurélien Grimaud <a.grimaud@involt.io>
2026-07-18 17:21:51 +0200

    feat: adjust Mixpanel session recording percentage based on environment

--- src/index.tsx
+++ src/index.tsx
@@ -48,7 +48,7 @@ if (globalConfig.env.isMixpanelEnabled) {
     disable_cookie: true,
     ignore_dnt: true,
     opt_out_tracking_by_default: true,
-    record_sessions_percent: 50,
+    record_sessions_percent: globalConfig.env.isDev ? 0 : 50,
     record_mask_text_selector: '*',
     record_block_selector: 'img',
     record_heatmap_data: true,
